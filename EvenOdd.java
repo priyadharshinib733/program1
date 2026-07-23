9cc3fb893d2841f87bec41c55ab0a2621509a4f1
Priyadharshini B <priyadharshini.b2024b@vitstudent.ac.in>
2026-07-23 15:29:05 +0530

    Update and rename README.md to EvenOdd.java

--- EvenOdd.java
+++ EvenOdd.java
@@ -3,12 +3,11 @@ import java.util.Scanner;
 public class EvenOdd {
 Scanner scan = new Scanner(System.in);
 System.out.print("Enter a number:");
-int num = sc.nextInt();
+int num = scan.nextInt();
 if(num%2==0)
 System.out.println("Even");
 else
 System.out.println("Odd");
-sc.close();
+scan.close();
 }
-
 }   
